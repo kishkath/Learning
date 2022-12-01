8439cc47ff1c5c916d6fd29f8428a601362cecde
Kishkath <60026221+kishkath@users.noreply.github.com>
2022-12-01 22:09:28 +0530

    uploading files

---
+++ ML/Road Transport Dataset/prediction.py
@@ -0,0 +1,23 @@
+import joblib
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+data = pd.read_csv("Dataset\cleanedData.csv")
+
+def encoding(input_val,colName):
+    le = LabelEncoder()
+    values = data[colName].unique()
+    data[colName] = le.fit_transform(data[colName])
+    dicts = dict(zip(values, data[colName].unique()))
+    return dicts[input_val]
+
+def get_predictions(data,model):
+    '''
+    Returns the predictions of model
+    :param data: .csv/values
+    :param model: .joblib
+    :return: Slight Injury/Fatal Injury/Serious Injury
+    '''
+    return model.predict(data)
